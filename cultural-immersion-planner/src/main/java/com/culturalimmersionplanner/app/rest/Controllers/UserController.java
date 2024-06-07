d6cbe5ecb7a65947686c89ab24302bd01e0abde4
Ross Learned <rclearned1@hotmail.com>
2024-06-06 19:43:46 -0700

    Added delete and get mapping to the user controller

--- cultural-immersion-planner/src/main/java/com/culturalimmersionplanner/app/rest/Controllers/UserController.java
+++ cultural-immersion-planner/src/main/java/com/culturalimmersionplanner/app/rest/Controllers/UserController.java
@@ -3,12 +3,12 @@ package com.culturalimmersionplanner.app.rest.Controllers;
 import com.culturalimmersionplanner.app.rest.Models.User;
 import com.culturalimmersionplanner.app.rest.Repo.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/api/users")
 public class UserController {
 
     @Autowired
@@ -19,4 +19,19 @@ public class UserController {
         return userRepository.save(user);
     }
 
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userRepository.deleteById(id);
+    }
+
+    @GetMapping("/{id}")
+    public User getUser(@PathVariable Long id) {
+        return userRepository.findById(id).orElseThrow(() ->
+                new RuntimeException("User not found"));
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
 }
--- cultural-immersion-planner/src/main/java/com/culturalimmersionplanner/app/rest/Repo/UserRepository.java
+++ cultural-immersion-planner/src/main/java/com/culturalimmersionplanner/app/rest/Repo/UserRepository.java
@@ -3,6 +3,4 @@ package com.culturalimmersionplanner.app.rest.Repo;
 import com.culturalimmersionplanner.app.rest.Models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
+public interface UserRepository extends JpaRepository<User, Long> { }
