d3bb9e07619a1d2c45c09298f557ccf903bb1585
rlearned <rclearned1@hotmail.com>
2024-06-07 21:41:13 -0700

    Experience controller and repository added

--- cultural-immersion-planner/src/main/java/com/culturalimmersionplanner/app/rest/Controllers/ExperienceController.java
+++ cultural-immersion-planner/src/main/java/com/culturalimmersionplanner/app/rest/Controllers/ExperienceController.java
@@ -1,8 +1,37 @@
 package com.culturalimmersionplanner.app.rest.Controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.culturalimmersionplanner.app.rest.Repo.ExperienceRepository;
+import com.culturalimmersionplanner.app.rest.Models.Experience;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/api/experiences")
 public class ExperienceController {
 
+    @Autowired
+    private ExperienceRepository experienceRepository;
+
+    @PostMapping
+    public Experience createExperience(@RequestBody Experience experience) {
+        return experienceRepository.save(experience);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteExperience(@PathVariable Long id) {
+        experienceRepository.deleteById(id);
+    }
+
+    @GetMapping("/{id}")
+    public Experience getExperience(@PathVariable Long id) {
+        return experienceRepository.findById(id).orElseThrow(() ->
+                new RuntimeException("Experience not found"));
+    }
+
+    @GetMapping
+    public List<Experience> getAllExperiences() {
+        return experienceRepository.findAll();
+    }
 }
--- cultural-immersion-planner/src/main/java/com/culturalimmersionplanner/app/rest/Repo/ExperienceRepository.java
+++ cultural-immersion-planner/src/main/java/com/culturalimmersionplanner/app/rest/Repo/ExperienceRepository.java
@@ -1,2 +1,6 @@
-package com.culturalimmersionplanner.app.rest.Repo;public class ExperienceRepository {
-}
+package com.culturalimmersionplanner.app.rest.Repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.culturalimmersionplanner.app.rest.Models.Experience;
+
+public interface ExperienceRepository extends JpaRepository<Experience, Long> { }
